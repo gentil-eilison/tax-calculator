25f098a5bc653b9570f3701c9c09783189703469
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-09-03 15:15:03 -0300

    isolates read file and string to float convertion logic

--- prices/prices.go
+++ prices/prices.go
@@ -1,10 +1,10 @@
 package prices
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
+
+	"github.com/gentil-eilison/tax-calculator/conversion"
+	"github.com/gentil-eilison/tax-calculator/filemanager"
 )
 
 type TaxIncludedPriceJob struct {
@@ -14,43 +14,18 @@ type TaxIncludedPriceJob struct {
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
-	file, err := os.Open("prices.txt")
+	lines, err := filemanager.ReadLines("prices.txt")
 
 	if err != nil {
-		fmt.Println("Couldn't open file!")
 		fmt.Println(err)
 		return
 	}
 
-	scanner := bufio.NewScanner(file)
-	var lines []string
-
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	err = scanner.Err()
+	prices, err := conversion.StringsToFloats(lines)
 
 	if err != nil {
-		fmt.Println("Reading the file content failed.")
 		fmt.Println(err)
-		file.Close()
 		return
-	}
-
-	prices := make([]float64, len(lines))
-
-	for lineIndex, line := range lines {
-		floatPrice, err := strconv.ParseFloat(line, 64)
-
-		if err != nil {
-			fmt.Println("Converting string to float failed.")
-			fmt.Println(err)
-			file.Close()
-			return
-		}
-
-		prices[lineIndex] = floatPrice
 	}
 
 	job.InputPrices = prices
