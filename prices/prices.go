d510ac2c4c48a308cb1473e5357c0116f838fa40
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-09-03 15:30:42 -0300

    writes jobs data to json files

--- prices/prices.go
+++ prices/prices.go
@@ -10,7 +10,7 @@ import (
 type TaxIncludedPriceJob struct {
 	TaxRate float64
 	InputPrices []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices map[string]string
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
@@ -40,7 +40,9 @@ func (job *TaxIncludedPriceJob) Process() {
 		result[key] = fmt.Sprintf("%.2f", taxIncludedPrice)
 	}
 
-	fmt.Println(result)
+	job.TaxIncludedPrices = result
+
+	filemanager.WriteJSON(fmt.Sprintf("result_%.0f.json", job.TaxRate * 100), job)
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
