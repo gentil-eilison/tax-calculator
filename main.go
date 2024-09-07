6f0bbbc877780b87a3f8c631c64281a1fee4faf2
Gentil Alyson <fernandes.gentilalysonjac@gmail.com>
2024-09-06 21:40:07 -0300

    makes use of goroutines

--- main.go
+++ main.go
@@ -9,16 +9,22 @@ import (
 
 func main() {
 	taxRates := []float64{0.0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
 
-	for _, taxRate := range taxRates {
+	for index, taxRate := range taxRates {
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate * 100))
+		doneChans[index] = make(chan bool)
 		// cmd := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		err := priceJob.Process()
+		go priceJob.Process(doneChans[index])
 
-		if err != nil {
-			fmt.Println("Could not process job")
-			fmt.Println(err)
-		}
+		// if err != nil {
+		// 	fmt.Println("Could not process job")
+		// 	fmt.Println(err)
+		// }
+	}
+
+	for _, doneChan := range doneChans {
+		<- doneChan
 	}
 }
